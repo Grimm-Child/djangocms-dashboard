fb23899ee35bf782282d457435a6c63cd3f7e951
Grimm-Child <GrimmStar.Graphics@gmail.com>
2020-03-05 18:57:24 -0500

    Make plugin compatible with Python 3.8 and Django CMS 3.7 with modifications to the url.py file

--- djangocms_dashboard/urls.py
+++ djangocms_dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from djangocms_dashboard.views import home, plugins_list, apphooks_list, plugin_detail, apphook_detail
 
 urlpatterns = [
-    url(r'^$', 'djangocms_dashboard.views.home', name='home'),
-    url(r'^plugins/$', 'djangocms_dashboard.views.plugins_list', name='plugins_list'),
-    url(r'^apphooks/$', 'djangocms_dashboard.views.apphooks_list', name='apphooks_list'),
-    url(r'^plugin_detail/(?P<plugin_type>\w+)$', 'djangocms_dashboard.views.plugin_detail', name='plugin_detail'),
-    url(r'^apphook_detail/(?P<apphook_class>\w+)$', 'djangocms_dashboard.views.apphook_detail', name='apphook_detail'),
+    url(r'^$', home, name='home'),
+    url(r'^plugins/$', plugins_list, name='plugins_list'),
+    url(r'^apphooks/$', apphooks_list, name='apphooks_list'),
+    url(r'^plugin_detail/(?P<plugin_type>\w+)$', plugin_detail, name='plugin_detail'),
+    url(r'^apphook_detail/(?P<apphook_class>\w+)$', apphook_detail, name='apphook_detail'),
     # url(r'^plugins/export/csv/$', 'djangocms_dashboard.views.export', name='export_csv'),
 ]
